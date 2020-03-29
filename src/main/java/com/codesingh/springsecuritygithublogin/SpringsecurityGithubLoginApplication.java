d4a312b0d981d242f93fe8ecd3b23d957b95d2ef
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-28 22:56:56 -0400

    starter project pushed

---
+++ src/main/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplication.java
@@ -0,0 +1,13 @@
+package com.codesingh.springsecuritygithublogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityGithubLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesingh.springsecuritygithublogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityGithubLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
