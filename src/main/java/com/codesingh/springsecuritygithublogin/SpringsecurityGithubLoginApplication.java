c62678a7bbef19b855704a5ed7d512a7d22c052a
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-29 00:30:45 -0400

    added oauth2 dependency

--- src/main/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplication.java
+++ src/main/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplication.java
@@ -2,8 +2,10 @@ package com.codesingh.springsecuritygithublogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringsecurityGithubLoginApplication {
 
 	public static void main(String[] args) {
