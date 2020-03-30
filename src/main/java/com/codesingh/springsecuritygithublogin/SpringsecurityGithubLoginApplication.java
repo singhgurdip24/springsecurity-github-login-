7dc00a96924b3c9ea51c9efbabd317b575a3a024
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-29 20:37:46 -0400

    oauth2 implemented

--- src/main/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplication.java
+++ src/main/java/com/codesingh/springsecuritygithublogin/SpringsecurityGithubLoginApplication.java
@@ -1,8 +1,14 @@
 package com.codesingh.springsecuritygithublogin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
